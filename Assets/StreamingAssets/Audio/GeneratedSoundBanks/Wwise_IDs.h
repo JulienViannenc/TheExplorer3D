b4ebde219692fd9e3a035b30160699d946fe1810
Nino <ninohericher@gmail.com>
2026-01-07 20:51:00 +0100

    ⚠️[Unity/Wwise] - Add Landing Sound but need to fix error

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,28 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ATTACKBEGIN = 2508816088U;
+        static const AkUniqueID ATTACKEND = 1639835052U;
         static const AkUniqueID DUCKSOUND = 3169408195U;
+        static const AkUniqueID FALL = 2512384458U;
+        static const AkUniqueID FTS_IDLE = 278299119U;
+        static const AkUniqueID FTS_RUN = 2027509688U;
+        static const AkUniqueID FTS_WALK = 3144061710U;
+        static const AkUniqueID GRUNT = 3797621065U;
+        static const AkUniqueID HIT = 1116398592U;
+        static const AkUniqueID LANDING = 2548270042U;
+        static const AkUniqueID SPOTTED = 3214304800U;
+        static const AkUniqueID STOP_ATTACKALL = 1328128341U;
+        static const AkUniqueID STOP_ATTACKBEGIN = 2837855987U;
+        static const AkUniqueID STOP_ATTACKEND = 1293984723U;
         static const AkUniqueID STOP_DUCKSOUND = 973396280U;
+        static const AkUniqueID STOP_FALL = 2695680267U;
+        static const AkUniqueID STOP_FTS_RUN = 3463244419U;
+        static const AkUniqueID STOP_FTS_WALK = 1280223711U;
+        static const AkUniqueID STOP_FTSIDLE = 2885577723U;
+        static const AkUniqueID STOP_GRUNT = 1848048310U;
+        static const AkUniqueID STOP_HIT = 300122239U;
+        static const AkUniqueID STOP_SPOTTED = 2765578991U;
     } // namespace EVENTS
 
     namespace BANKS
